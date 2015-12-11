ec29671380f2c2d1986bb4cf81b543ae3018f112
dwandrowec <dwandrowec@gmail.de>
2015-12-11 13:51:16 +0100

    Changed Statistics

---
+++ app/src/main/java/com/example/wlg/ratewer/API/Hello.java
@@ -0,0 +1,20 @@
+package com.example.wlg.ratewer.API;
+
+/**
+ * Created by RareHue on 11.12.2015.
+ */
+
+public class Hello {
+
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+}

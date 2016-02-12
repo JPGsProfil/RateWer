38ee3d93d77d2e20b46c3563366c5cd396f75e6c
Jean <j.gnauck@gmx.net>
2016-02-12 19:33:24 +0100

    removed dummy from Statistic-Activity placed Tectviews for name, amount won and lost

--- app/src/main/java/com/example/wlg/ratewer/Activities/StatisticActivity.java
+++ app/src/main/java/com/example/wlg/ratewer/Activities/StatisticActivity.java
@@ -1,10 +1,9 @@
 package com.example.wlg.ratewer.Activities;
 
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.widget.ExpandableListView;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-import com.example.wlg.ratewer.Adapter.ExpandableListAdapter;
 import com.example.wlg.ratewer.R;
 
 import java.util.ArrayList;
@@ -12,73 +11,25 @@ import java.util.HashMap;
 import java.util.List;
 
 
-public class StatisticActivity extends ActionBarActivity {
+public class StatisticActivity extends AppCompatActivity {
 
-    ExpandableListAdapter listAdapter;
-    ExpandableListView expListView;
-    List<String> listDataHeader;
-    HashMap<String, List<String>> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistic);
+        TextView tv_Name = (TextView) findViewById(R.id.tv_statistic_Name);
+        tv_Name.setText("du");
 
-        // get the listview
-        expListView = (ExpandableListView) findViewById(R.id.lvExp);
+        TextView tv_Won = (TextView) findViewById(R.id.tv_statistic_Won);
+        tv_Won.setText("0 mal");
 
-        // preparing list data
-        prepareListData();
+        TextView tv_Lost = (TextView) findViewById(R.id.tv_statistic_Lost);
+        tv_Lost.setText("0 mal");
 
-        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
 
-        // setting list adapter
-        expListView.setAdapter(listAdapter);
-    }
 
-    /*
-     * Preparing the list data
-     */
-    private void prepareListData() {
-        listDataHeader = new ArrayList<String>();
-        listDataChild = new HashMap<String, List<String>>();
-
-        // Adding child data
-        listDataHeader.add("Top 250");
-        listDataHeader.add("Now Showing");
-        listDataHeader.add("Coming Soon..");
-
-        // Adding child data
-        List<String> top250 = new ArrayList<String>();
-        top250.add("The Shawshank Redemption");
-        top250.add("The Godfather");
-        top250.add("The Godfather: Part II");
-        top250.add("Pulp Fiction");
-        top250.add("The Good, the Bad and the Ugly");
-        top250.add("The Dark Knight");
-        top250.add("12 Angry Men");
-
-        List<String> nowShowing = new ArrayList<String>();
-        nowShowing.add("The Conjuring");
-        nowShowing.add("Despicable Me 2");
-        nowShowing.add("Turbo");
-        nowShowing.add("Grown Ups 2");
-        nowShowing.add("Red 2");
-        nowShowing.add("The Wolverine");
-
-        List<String> comingSoon = new ArrayList<String>();
-        comingSoon.add("2 Guns");
-        comingSoon.add("The Smurfs 2");
-        comingSoon.add("The Spectacular Now");
-        comingSoon.add("The Canyons");
-        comingSoon.add("Europa Report");
-
-        System.out.println("listDataHeader.get(0) "+listDataHeader.get(0));
-
-        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
-        listDataChild.put(listDataHeader.get(1), nowShowing);
-        listDataChild.put(listDataHeader.get(2), comingSoon);
     }
 }
 
